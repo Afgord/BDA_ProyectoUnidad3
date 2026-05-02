88043372179fd5e98d681607e1b6257db73ecca9
Afgord <christian.martinez@potros.itson.edu.mx>
2026-05-01 18:53:49 -0700

    creación de módulos

---
+++ ReservaTrenesMongoDB/src/main/java/itson/reservatrenesmongodb/ReservaTrenesMongoDB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.reservatrenesmongodb;
+
+/**
+ *
+ * @author Afgord
+ */
+public class ReservaTrenesMongoDB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
